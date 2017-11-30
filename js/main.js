34b3116b0c5c7807b1f04cb407dbefaea0db0309
Jake Chavez <jakechavez@jakes-mbp.attlocal.net>
2017-11-30 14:53:18 -0600

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
-var cards = ["queen", "queen", "king", "king"]
+var cards = [
+{
+rank:"queen",
+suit:"hearts",
+cardImage:"images/queen-of-hearts.png",
+},
+{
+rank:"queen",
+suit:"diamonds",
+cardImage:"images/queen-of-diamonds.png",
+},
+{
+rank:"king",
+suit:"hearts",
+cardImage:"images/king-of-hearts.png",
+},
+{
+rank:"king",
+suit:"diamonds",
+cardImage:"images/king-of-diamonds.png",
+},
+]
 
 var cardsInPlay = [];
 
@@ -13,8 +34,10 @@ var checkForMatch = function() {
 }
 
 var flipCard = function(cardID) {
-console.log("User flipped " + cards[cardID]);
-cardsInPlay.push(cards[cardID])
+console.log("User flipped " + cards[cardID].rank);
+console.log(cards[cardID].cardImage);
+console.log(cards[cardID].suit);
+cardsInPlay.push(cards[cardID].rank);
 checkForMatch();
 }
 flipCard(0);
